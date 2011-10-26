2e59f486f1739acd150a521c62cac13c6242f514
Noah Goodrich <noah.goodrich@lendio.com>
2011-10-26 14:37:23 -0600

    Completely reinvented the Table library. Way simpler complete separation of table rendering from javascript for datatables

--- library/views/dom.php
+++ library/views/dom.php
@@ -7,43 +7,21 @@
  */
  ?>
 
-<? if(!empty($this->_add)): ?>
-	<a href="<?= $this->_baseUrl.''.$this->_add['url']?>" class="<?= join(' ', $this->_add['classes']); ?>">
-		<span><?= $this->_add['label'] ?></span>
-	</a>
-<? endif; ?>
-
-<table id="<?= $this->_name ?>" class="display">
+<table id="<?= $this->_attr['id'] ?>" class="display">
 	<thead>
 	<tr>
-	<? foreach($this->columns() as $column): ?>
+	<? foreach($this->_columns as $column): ?>
 		<th><?= $column->header() ?></th>
 	<? endforeach; ?>
-	<? if(count($this->actions())): ?>
-		<th>Options</th>
-	<? endif; ?>
 	</tr>
 	</thead>
 	<tbody>
 	<? foreach($this->_data as $tuple): ?>
 	<tr>
-	<? foreach($this->columns() as $column): ?>
-		<td><?= $column->render($tuple) ?></td>
+	<? foreach($this->_columns as $column): ?>
+		<?= $column->render($tuple) ?>
 	<? endforeach; ?>
-	<? if(count($this->actions())): ?>
-		<td>
-		<? foreach($this->actions() as $option): ?>
-			<?= $option->render($tuple) ?>
-		<? endforeach ?>
-		</td>
-	<? endif; ?>
 	</tr>
 	<? endforeach; ?>
 	</tbody>
 </table>
-
-<script>
-$(document).ready(function() {
-	$('#<?= $this->_name ?>').dataTable();
-});
-</script>
