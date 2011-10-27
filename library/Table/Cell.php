1afa1179b401f034614e4dbc9d935272eeafdb16
Noah Goodrich <noah.goodrich@lendio.com>
2011-10-27 09:57:17 -0600

    Added missing files

---
+++ library/Table/Cell.php
@@ -0,0 +1,73 @@
+<?php
+/** 
+ * @author noah
+ * @date 3/8/11
+ * @brief
+ * 
+*/
+
+namespace Table;
+
+use Table\Value as Value;
+
+class Cell {
+
+	protected $_baseUrl;
+	
+	protected $_valueObj;
+
+	protected $_value;
+
+	protected $_header;
+
+	protected $_classes;
+
+	public function __construct(array $config)
+	{
+		$this->_valueObj = new Value;
+		
+		foreach($config as $k => $v) {
+			switch($k) {
+				case 'value':
+					$this->_value = $v;
+					break;
+				case 'header':
+					$this->_header = $v;
+					break;
+				case 'baseUrl':
+					$this->_baseUrl = $v;
+					break;
+			}
+		}
+	}
+
+	/**
+	 * @param  $object
+	 * @return string
+	 */
+	public function value($object)
+	{
+		$meth = $this->_valueObj;
+
+		return $meth($this->_value, $object);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function header()
+	{
+		$meth = $this->_valueObj;
+
+		return $meth($this->_header, func_get_args());
+	}
+
+	/**
+	 * @param  $object
+	 * @return void
+	 */
+	public function render($object)
+	{
+		return '<td>'.$this->value($object).'</td>';
+	}
+}
---
+++ samples/dom.php
@@ -0,0 +1,27 @@
+<?php
+
+function debug($var)
+{
+	echo '<pre>'.print_r($var, true).'</pre>';
+}
+
+require __DIR__.'/../library/Table.php';
+
+spl_autoload_register(array('Table', 'autoload'));
+
+$db = new PDO('mysql:dbname=world;host=localhost', 'table', 'table');
+
+$rows = $db->query('select * from City', PDO::FETCH_OBJ);
+
+$table = new Table;
+
+$table->add(
+		array(
+			array('header' => 'ID', 'value' => function($o) { return $o->ID;}),
+			array('header' => function() { return 'Name'; }, 'value' => function($o) {return $o->Name; }),
+			array('value' => 'Test Value')
+		)			
+	)
+	->setDataSource($rows);
+	
+echo $table->render();
